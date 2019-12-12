1525274a80abdb070d771edfc860bcff72e80965
Singham <rahul.singh4800@gmail.com>
2019-12-12 18:53:18 +0530

    wallpaper testcase added

--- app/wallpaper/tests.py
+++ app/wallpaper/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import Wallpaper
+
+class WallpaperTestCase(TestCase):
+    def setup(self):
+        Wallpaper.objects.create()
+        pass
